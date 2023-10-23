4f75cc44c8ce09dc830fa075db7c948f706fe11f
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-23 14:52:26 +0000

    adding component into application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello React in Rails! It's really funny.</h1>
+            <p>Small paragraph</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
